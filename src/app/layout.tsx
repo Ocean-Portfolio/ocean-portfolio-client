d13047124e25e5bd0e76c3ec7b16a288c5b25d62
steven-yn <ssungyeon94@gmail.com>
2023-09-18 22:08:41 +0900

    chore: stitches 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { getCssText } from "../../stitches.config";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <style
+          id="stitches"
+          dangerouslySetInnerHTML={{ __html: getCssText() }}
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
